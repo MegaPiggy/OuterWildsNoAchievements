4cd63c64d1a60a3e4554a8085215c47e3004e31a
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-02-14 12:09:11 -0500

    Add files

---
+++ NoAchievements.cs
@@ -0,0 +1,14 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace NoAchievements
+{
+    public class NoAchievements : ModBehaviour
+    {
+        private void Start()
+        {
+            ModHelper.HarmonyHelper.EmptyMethod<Achievements>(nameof(Achievements.Earn));
+        }
+    }
+
+}
